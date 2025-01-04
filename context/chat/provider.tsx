53a307151d9ad3ae9e49f547205d2180490ea0b9
Nischal Bhattarai <nishchalb21@gmail.com>
2025-01-04 17:31:28 +0545

    feat: add ChatProvider and context for managing chat sessions

---
+++ context/chat/provider.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+
+import { ChatContext } from "./context";
+export type TChatProvider = {
+    children: React.ReactNode
+}
+
+
+
+export const ChatProvider = ({ children }: TChatProvider) => {
+    return (
+        <ChatContext.Provider value={{ chatSession: [] }}>
+            {children}
+        </ChatContext.Provider>
+    );
+}
