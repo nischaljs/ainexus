23c79a7ad11c516bd9bf17ed397338f9a1d0b72b
Nischal Bhattarai <nishchalb21@gmail.com>
2025-01-16 20:29:12 +0545

    .....

--- context/filter/provider.tsx
+++ context/filter/provider.tsx
@@ -1,24 +1,35 @@
 "use client"
 
-import { ReactNode, useState } from "react"
-import { FiltersContext } from "./context"
-import { CommandDialog, CommandInput } from "@/components/ui/command"
+import { reactnode, usestate } from "react"
+import { filterscontext } from "./context"
+import { commanddialog, commandempty, commandgroup, commandinput, commanditem, commandlist } from "@/components/ui/command"
+import { usechatsession } from "@/hooks/use-chat-session"
 
-export type TFiltersProvider = {
-    children: ReactNode
+export type tfiltersprovider = {
+    children: reactnode
 }
 
-export const FiltersProvider = ({ children }: TFiltersProvider) => {
-    const [isFilterOpen, setIsFilterOpen] = useState(false)
+export const filtersprovider = ({ children }: tfiltersprovider) => {
+    const [isfilteropen, setisfilteropen] = usestate(false)
 
-    const open = () => setIsFilterOpen(true)
-    const dismiss = () => setIsFilterOpen(false)
+    const open = () => setisfilteropen(true);
+    const dismiss = () => setisfilteropen(false);
+    const {sessions, createsessions} = usechatsession()
+
 
     return (
-        <FiltersContext.Provider value={{ open, dismiss }}>
+        <filterscontext.provider value={{ open, dismiss }}>
             {children}
             <CommandDialog open={isFilterOpen} onOpenChange={setIsFilterOpen}>
                 <CommandInput placeholder="Search ...." />
+                <CommandList>
+                    <CommandEmpty> No results found  </CommandEmpty>
+                    <CommandGroup headings="Actions">
+                        <CommandItem className="gap-3" value="new" onselect={()=>{
+
+                        }}>
+                    </CommandGroup>
+                </CommandList>
             </CommandDialog>
         </FiltersContext.Provider>
     )
